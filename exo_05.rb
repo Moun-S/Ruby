0be4692e00bc53a2e213fd372465043ef2d240d7
Mounir <sekkalmounir@gmail.com>
2020-04-28 14:39:35 +0200

    #{} interpolation variable dans une string

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" #Affichage string
+puts "Travail : #{10 * 5 * 11}" # Affichage string + Résultat multiplication des nombres (interpolation d'une variable dans une string)
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" # Affichage string + Résultat multiplication des nombres (interpolation d'une variable dans une string)
+
+puts "Et en secondes ?" #Affichage string
+
+puts 10 * 5 * 11 * 60 * 60 #Affichage résultat multiplication nombres
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #Affichage string
+
+puts 3 + 2 < 5 - 7 #Vérification et affichage true/false
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" #Addition des nombres (interpolation d'une variable dans une string)
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" #Soustraction des nombres (interpolation d'une variable dans une string)
+
+puts "Ok, c'est faux alors !" #Affichage string
+
+puts "C'est drôle ça, faisons-en plus :" #AFichage string
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #Vérification et affichage true/false (interpolation d'une variable dans une string)
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #Vérification et affichage true/false (interpolation d'une variable dans une string)
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #Vérification et affichage true/false (interpolation d'une variable dans une string)
